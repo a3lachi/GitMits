3c026dd801dd04edccc8309e49d61d3f07ecebac
a3laxi <mad.aalachi@gmail.com>
2023-04-02 02:48:58 +0200

    Initial Commit

---
+++ GitMits/GitMitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitMitsApp.swift
+//  GitMits
+//
+//  Created by Mli7 on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitMitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
